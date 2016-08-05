3e2564c194ef06354f54d8a6cc4b22c5427b3033
ulloav <vulloa@csumb.edu>
2016-08-05 11:15:42 -0700

    recursion - factorial

--- src/Recursion_1/CodingBatRecursion1.java
+++ src/Recursion_1/CodingBatRecursion1.java
@@ -9,6 +9,136 @@ public class CodingBatRecursion1 {
     {
         System.out.println("CodingBat Recursion-1");
         System.out.println("Complete 25 out of 30 Exercises.\n");
+
+        /// factorial ///
+        System.out.println("\n/// factorial ///");
+
+        int factorialInt = 1;
+        //int factorialInt = 2;
+        //int factorialInt = 6;
+
+        System.out.println("factorial: " + factorial.factorial(factorialInt));
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
+        ///  ///
+        System.out.println("\n///  ///");
+
+        System.out.println("");
+
     }
 
 }
---
+++ src/Recursion_1/factorial.java
@@ -0,0 +1,13 @@
+package Recursion_1;
+
+/**
+ * Created by ulloav on 8/5/2016.
+ */
+public class factorial {
+
+    public static int factorial(int n)
+    {
+
+    }
+
+}
