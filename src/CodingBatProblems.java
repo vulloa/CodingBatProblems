e5a9ed4e02ea5ad6eda2da38cb5edf7a6992e8e3
ulloav <vulloa@csumb.edu>
2016-08-05 10:56:14 -0700

    remove uncessary packages/problems

--- src/CodingBatProblems.java
+++ src/CodingBatProblems.java
@@ -1,8 +1,6 @@
 import AP_1.CodingBatAP1;
 import Recursion_1.CodingBatRecursion1;
-import Recursion_2.CodingBatRecursion2;
 import String_2.CodingBatString2;
-import String_3.CodingBatString3;
 
 /**
  * Created by ulloav on 7/25/2016.
@@ -11,26 +9,19 @@ public class CodingBatProblems {
 
     public static void main(String[] args)
     {
-        //  16 problems completed on Coding Bat AP-1 section
+        /*
         CodingBatAP1 answers_one = new CodingBatAP1();
 
         System.out.print("\n\n");
 
         CodingBatString2 answers_two = new CodingBatString2();
-/*
-        System.out.print("\n\n");
-
-        CodingBatString3 answers_three = new CodingBatString3();
 
         System.out.print("\n\n");
+        */
 
         CodingBatRecursion1 answers_four = new CodingBatRecursion1();
 
         System.out.print("\n\n");
 
-        CodingBatRecursion2 answers_five = new CodingBatRecursion2();
-
-        System.out.print("\n\n");
-*/
     }
 }
